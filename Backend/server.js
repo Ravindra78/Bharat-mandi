4f35b9d47f14c55d37cd95c28b3b88c3dbd5d3cc
Ravindra Prajapati <tumharaemail@gmail.com>
2026-02-06 16:56:04 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.json({ message: 'Bharat Mandi API' });
+	res.json({ message: 'Bharat Mandi' });
 });
 
 app.get('/health', (req, res) => res.send('OK'));
