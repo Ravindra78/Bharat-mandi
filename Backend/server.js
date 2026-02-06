b90153ada3c75de531c0b7ba50b30706a96d159a
Ravindra Prajapati <tumharaemail@gmail.com>
2026-02-06 22:47:47 +0530

    change schema

--- Backend/server.js
+++ Backend/server.js
@@ -47,6 +47,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+	console.log(`Server is running  on port ${port}`);
 });
 
